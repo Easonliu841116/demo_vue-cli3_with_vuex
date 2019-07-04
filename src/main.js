fb91941851bc8dd173dedcf97b6c23d9b8ce60ab
Eason Liu <a0956031116@gmail.com>
2019-07-05 01:40:07 +0800

    add new project

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 import Vue from 'vue';
-import App from './App.vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/vue-loading.css';
+import 'bootstrap';
 import router from './router';
 import store from './store';
+import App from './App.vue';
 
 Vue.config.productionTip = false;
+Vue.use(VueAxios, axios);
+Vue.component('Loading', Loading);
 
 new Vue({
   router,
